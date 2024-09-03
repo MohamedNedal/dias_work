29ddf6eb2d52ac0e49d9b3e3031fc42b76cee098
MohamedNedal <mohamed.nedal.solar@gmail.com>
2024-09-03 17:05:40 +0100

    bash scripts

--- png_to_mp4.sh
+++ png_to_mp4.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 # Change the names of the folders to the desired location
-images_folder = 'suvi/20240514'
-channel = '171A'
+images_folder = 'aia' #'suvi/20240514'
+channel = '193A'
 # 131A, 171A, 195A, 284A, 304A, 94A
 
 # Navigate to the directory containing your images
-cd ~/data/output_png/'$images_folder'/'$channel'
+cd ~/data/png/'$images_folder'/'$channel'
 
 # List the images in natural sort order and save to images.txt
 ls -1v *.png > images.txt
@@ -14,5 +16,4 @@ ls -1v *.png > images.txt
 sed 's|^|file |' images.txt > formatted_images.txt
 
 # Create the video using ffmpeg
-ffmpeg -f concat -safe 0 -i formatted_images.txt -c:v libx264 -r 12 -pix_fmt yuv420p output_'$channel'_12fps.mp4
-
+ffmpeg -f concat -safe 0 -i formatted_images.txt -c:v libx264 -r 12 -pix_fmt yuv420p output_AIA'$channel'_12fps.mp4
